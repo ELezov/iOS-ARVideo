88def7947624eab0b571be4a9202ce1258c16f9c
ELezov <eugene.lezov@gmail.com>
2019-06-03 16:56:44 +0200

    Initial Commit

---
+++ iOS-ARVideo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOS-ARVideo
+//
+//  Created by EugenKGD on 03/06/2019.
+//  Copyright © 2019 ELezov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
